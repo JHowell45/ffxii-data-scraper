9ff300f258beac1b399d8f0d18c69836fe03d295
Jacob <jacob@notactivelylooking.com>
2023-02-16 01:51:50 +0000

    Creating separate function for magic and adding progress bar.

--- run.py
+++ run.py
@@ -1,6 +1,33 @@
-from src.magic.scraper import ArcaneMagickPage
+from src.magic.scraper import (
+    ArcaneMagickPage,
+    BlackMagickPage,
+    GreenMagickPage,
+    TimeMagickPage,
+    WhiteMagickPage,
+)
+
+from alive_progress import alive_bar
+
+from src.serialiser import Serialiser
+
+
+def save_magick(filepath: str):
+    magicks = []
+    models = [
+        ArcaneMagickPage(),
+        BlackMagickPage(),
+        GreenMagickPage(),
+        TimeMagickPage(),
+        WhiteMagickPage(),
+    ]
+    with alive_bar(len(models)) as bar:
+        for scraper in models:
+            scraper.load()
+            magicks += scraper.get_magicks().serialise()
+            bar()
+    serial = Serialiser(filepath)
+    serial.save(magicks)
 
 
 if __name__ == "__main__":
-    arcane_scraper = ArcaneMagickPage()
-    arcane_scraper.load_magicks_table()
+    save_magick("./magick.json")
