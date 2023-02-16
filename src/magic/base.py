88e3a53d1cce3d729dd848ff19f5a0c3426e9462
Jacob <jacob@notactivelylooking.com>
2023-02-16 01:52:45 +0000

    Moving Where to find to a shared module.

--- src/magic/base.py
+++ src/magic/base.py
@@ -1,32 +1,15 @@
 from dataclasses import dataclass, field
 from typing import List
 from enum import Enum
+from src.shared import WhereToFind
 
 
 class MagickType(Enum):
-    ARCANE: 1
-    BLACK: 2
-    GREEN: 3
-    TIME: 4
-    WHITE: 5
-
-
-@dataclass
-class WhereToFind:
-    purchased: List[str] = field(default_factory=list)
-    treasure: List[str] = field(default_factory=list)
-
-    def add_purchased(self, purchase: str):
-        self.purchased.append(purchase)
-
-    def add_treasure(self, treasure: str):
-        self.treasure.append(treasure)
-
-    def serialise(self) -> dict:
-        return {
-            "purchased": self.purchased,
-            "treasure": self.treasure,
-        }
+    ARCANE = "arcane"
+    BLACK = "black"
+    GREEN = "green"
+    TIME = "time"
+    WHITE = "white"
 
 
 @dataclass
